5328072e850f710563f8d4d2894c015fb381ba8b
Miles McLeod <milesmcleod123@gmail.com>
2017-10-23 15:34:48 -0700

    Complete phase 2

--- art_share_api/app/controllers/artworks_controller.rb
+++ art_share_api/app/controllers/artworks_controller.rb
@@ -1,26 +1,53 @@
 class ArtworksController < ApplicationController
 
   def index
-    
+    @artworks = Artwork.all
+    render json: @artworks
   end
 
   def show
-
+    @artwork = Artwork.find(params[:id])
+    if @artwork
+      render json: @artwork
+    else
+      render json: @artwork.errors.full_messages, status: 404
+    end
   end
 
   def create
-
+    @artwork = Artwork.new(artwork_params)
+    if @artwork.save
+      render json: @artwork
+    else
+      render json: @artwork.errors.full_messages, status: 422
+    end
   end
 
   def update
-
+    @artwork = Artwork.find(params[:id])
+    if @artwork
+      @artwork.update(artwork_params)
+      @artwork.save
+      render json: @artwork
+    else
+      render json: @artwork.errors.full_messages, status: 404
+    end
   end
 
   def destroy
-
+    @artwork = Artwork.find(params[:id])
+    if @artwork
+      @artwork.destroy
+      render json: @artwork
+    else
+      render json: @artwork.errors.full_messages, status: 404
+    end
   end
 
+  private
 
-
+  def artwork_params
+    params[:artwork].permit(:title, :image_url, :artist_id)
+  end
 
 end
